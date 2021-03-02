492801bacef855a5d397a680407ee60b1313b7ae
Charlie Greenman <charlie@razroo.com>
2021-03-02 03:21:33 +0200

    Upate logic for final file produced by article build.

--- build-articles.js
+++ build-articles.js
@@ -4,7 +4,9 @@ const articlesJson = require('./articles.json');
 
 let files = articlesJson.files;
 for (x in files){
-    let builtFilePath = `./build/articles/${files[x].split("/").pop()}`
+    let builtFilePath = `./build/articles/${files[x].split("/").pop()}`;
+    builtFilePath = builtFilePath.replace("md", "html");
+
     mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
     razrooMarkdownEngine(files[x], builtFilePath).then((output)=>{
         console.log(output)
