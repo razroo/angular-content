269bb06552286ef9b4cc15736fd5197308cd7037
Charlie Greenman <charlie@razroo.com>
2021-04-11 19:57:17 +0300

    Update logic to add UID to the articles.json file.

--- build-articles.js
+++ build-articles.js
@@ -1,27 +1,41 @@
 const razrooMarkdownEngine = require('@razroo/razroo-markdown-engine').resolveMarkdownFile;
 const mkdirp = require('mkdirp')
-const articlesJson = require('./articles.json');
+const articlesJsonName = './articles.json'
+const articlesJson = require(articlesJsonName);
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 let files = articlesJson.files;
+
+addUidIfNonePreset(files);
+
 for (x in files){
-    addUidIfNonePreset(files[x]);
     let builtFilePath = `./build/articles/${files[x].path.split("/").pop()}`;
     builtFilePath = builtFilePath.replace("md", "html");
 
     mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
     razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
-        console.log(output)
+      console.log(output)
     })
 }
 
-function addUidIfNonePreset(fileObject) {
-   if(fileObject.UID) {
-     return;
+function addUidIfNonePreset(files) {
+   for(let file in files){
+     let fileObject = files[file];
+
+     if(fileObject.UID) {
+       return;
+     }
+     else {
+       fileObject["UID"] = uuidv4();
+     }
    }
-   else {
-     fileObject.UID = uuidv4();
-   }
-   console.log('fileObject');
-   console.log(fileObject);
+
+   console.log('articlesJson');
+   console.log(articlesJson);
+   fs.writeFile(articlesJsonName, JSON.stringify(articlesJson, null, 2), function writeJSON(err) {
+      if (err) return console.log(err);
+      console.log(JSON.stringify(articlesJson));
+      console.log('writing to ' + articlesJsonName);
+   });
 }
