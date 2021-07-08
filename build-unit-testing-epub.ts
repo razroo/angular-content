52779518e59b5501c261e0f11dec28ae80863976
Charlie Greenman <charlie@razroo.com>
2021-07-09 01:40:06 +0300

    Add epub gen to app and update epub build.

--- build-unit-testing-epub.ts
+++ build-unit-testing-epub.ts
@@ -6,7 +6,9 @@ import {parse} from "node-html-parser";
 const files = bookJson.files;
 
 let content = [];
-for (let x in files) {
+let unitTestingArr = files.filter(file => file.title !== "Unit Testing");
+
+for (let x in  unitTestingArr) {
   let builtFilePath = `./build/book/${files[x].path.split("/").pop()}`;
   builtFilePath = builtFilePath.replace("md", "html");
   let htmlFileString = fs.readFileSync(builtFilePath, "utf-8");
@@ -26,7 +28,7 @@ const options = {
     publisher: "Razroo", // optional
     cover: "assets/angular-the-full-gamut-unit-testing-cover.jpg", // Url or File path, both ok.
     content: content,
-    output: './build/epub/angular-unit-testing.epub'
+    output: './build/epub/angular-unit-testingtitvl.epub'
 };
 
 new Epub(options).promise.then(
