edae3e0e07d471ddd30656cff589ac2cf208a470
Charlie Greenman <charlie@razroo.com>
2021-08-15 17:35:46 +0300

    Add axios to the repo.

---
+++ translate-articles.ts
@@ -0,0 +1,20 @@
+import {addBookIdIfNoneExists, addChapterTitleIfNonePresent} from "./strapi-scripts/content-helpers";
+
+const razrooMarkdownEngine = require('@razroo/razroo-markdown-engine').resolveMarkdownFile;
+const mkdirp = require('mkdirp')
+const bookJsonName = './book.json';
+const bookJson = require(bookJsonName);
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+let files = bookJson.files;
+
+for (let x in files){
+    let builtFilePath = `./build/book/${files[x].path.split("/").pop()}`;
+    builtFilePath = builtFilePath.replace("md", "html");
+
+    mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
+    razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
+      console.log(output);
+    })
+}
