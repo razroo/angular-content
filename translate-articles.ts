d35228f69acd75911b5173f920a7da24f1ca6823
Charlie Greenman <charlie@razroo.com>
2021-08-15 20:59:27 +0300

    Update the translate articles script to work.

--- translate-articles.ts
+++ translate-articles.ts
@@ -1,31 +1,36 @@
-import {addBookIdIfNoneExists, addChapterTitleIfNonePresent} from "./strapi-scripts/content-helpers";
-
 const razrooMarkdownEngine = require('@razroo/razroo-markdown-engine').resolveMarkdownFile;
 const mkdirp = require('mkdirp')
 const bookJsonName = './book.json';
 const bookJson = require(bookJsonName);
-const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const axios = require('axios');
 
 let files = bookJson.files;
 
-for (let x in files){
-    let builtFilePath = `./build/book/${files[x].path.split("/").pop()}`;
-    builtFilePath = builtFilePath.replace("md", "html");
+// for (let x in files){
+//     let builtFilePath = `./build/book/${files[x].path.split("/").pop()}`;
+//     builtFilePath = builtFilePath.replace("md", "html");
 
-    mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
-    razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
-      console.log(output);
-    })
-}
+//     mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
+//     razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
+//       console.log(output);
+//     })
+// }
 
 
 axios({
   method: 'post',
-  url: 'api-free.deepl.com/v2/translate'
+  url: 'https://api-free.deepl.com/v2/translate?auth_key=',
+  params: {
+    source_lang: 'EN',
+    target_lang: 'ES',
+    text: 'hello',
+  }
 }).then(response => {
     console.log('response');
-    console.log(response);
+    console.log(response.data.translations[0].text);
+  }).catch(error => {
+    console.log('error');
+    console.log(error);
   });
   
