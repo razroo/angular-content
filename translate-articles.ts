50d3e35024341a902c66ae4b2841e9cf9a044722
Charlie Greenman <charlie@razroo.com>
2021-08-15 21:14:28 +0300

    Complete demo for transliteration.

--- translate-articles.ts
+++ translate-articles.ts
@@ -4,33 +4,36 @@ const bookJsonName = './book.json';
 const bookJson = require(bookJsonName);
 const fs = require('fs');
 const axios = require('axios');
+import { readFileSync } from 'fs';
 
 let files = bookJson.files;
 
 // for (let x in files){
-//     let builtFilePath = `./build/book/${files[x].path.split("/").pop()}`;
-//     builtFilePath = builtFilePath.replace("md", "html");
+    // let builtFilePath = `./build/book/${files[0].path.split("/").pop()}`;
+    // let fileToBeBuilt = builtFilePath.replace("md", "html");
+    let fileString = readFileSync(files[0].path).toString();
 
-//     mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
-//     razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
-//       console.log(output);
-//     })
+    console.log('builtFilePath');
+    console.log(fileString);
+
+    axios({
+      method: 'post',
+      url: 'https://api-free.deepl.com/v2/translate?auth_key=',
+      params: {
+        source_lang: 'EN',
+        target_lang: 'ES',
+        text: fileString,
+      }
+    }).then(response => {
+        console.log('response');
+        console.log(response.data.translations[0].text);
+      }).catch(error => {
+        console.log('error');
+        console.log(error);
+      });
+
+    // razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
+    //   console.log(output);
+    // })
 // }
-
-
-axios({
-  method: 'post',
-  url: 'https://api-free.deepl.com/v2/translate?auth_key=',
-  params: {
-    source_lang: 'EN',
-    target_lang: 'ES',
-    text: 'hello',
-  }
-}).then(response => {
-    console.log('response');
-    console.log(response.data.translations[0].text);
-  }).catch(error => {
-    console.log('error');
-    console.log(error);
-  });
   
