39909b3a1d24642e0c96b5eaf90ecd253947b3e7
Charlie Greenman <charlie@razroo.com>
2021-08-29 00:51:27 +0300

    Add a translate-articles files to the repo.

--- translate-articles.ts
+++ translate-articles.ts
@@ -1,39 +1,35 @@
 const razrooMarkdownEngine = require('@razroo/razroo-markdown-engine').resolveMarkdownFile;
 const mkdirp = require('mkdirp')
-const bookJsonName = './book.json';
-const bookJson = require(bookJsonName);
 const fs = require('fs');
 const axios = require('axios');
-import { readFileSync } from 'fs';
+const articlesJsonName = './articles.json';
+const articlesJson = require(articlesJsonName);
+import { readFileSync, writeFileSync } from 'fs';
 
-let files = bookJson.files;
+let files = articlesJson.files;
 
-// for (let x in files){
-    // let builtFilePath = `./build/book/${files[0].path.split("/").pop()}`;
-    // let fileToBeBuilt = builtFilePath.replace("md", "html");
+// for (let x in files) {
+    let stagingPath = `./build/articles/es/${files[0].path.split("/").pop()}`;
     let fileString = readFileSync(files[0].path).toString();
-
-    console.log('builtFilePath');
-    console.log(fileString);
 
     axios({
       method: 'post',
-      url: 'https://api-free.deepl.com/v2/translate?auth_key=',
+      url: 'https://api.deepl.com/v2/translate?auth_key=',
       params: {
         source_lang: 'EN',
         target_lang: 'ES',
         text: fileString,
       }
     }).then(response => {
-        console.log('response');
-        console.log(response.data.translations[0].text);
+        mkdirp.sync(stagingPath.substring(0, stagingPath.lastIndexOf("/")))
+        writeFileSync(stagingPath, response.data.translations[0].text);
+        // razrooMarkdownEngine(response.data.translations[0].text, builtFilePath).then((output)=>{
+        //   console.log(output);
+        // });
       }).catch(error => {
         console.log('error');
         console.log(error);
       });
 
-    // razrooMarkdownEngine(files[x].path, builtFilePath).then((output)=>{
-    //   console.log(output);
-    // })
 // }
   
