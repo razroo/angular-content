c598a247ca4c13ff9510769650f0f04cafc5ff37
Charlie Greenman <charlie@razroo.com>
2021-08-29 01:20:30 +0300

    Add translated articles build to the angular-content repo.

---
+++ build-translated-articles.ts
@@ -0,0 +1,21 @@
+import {addUidIfNonePreset} from "./strapi-scripts/content-helpers";
+
+const razrooMarkdownEngine = require('@razroo/razroo-markdown-engine').resolveMarkdownFile;
+const mkdirp = require('mkdirp')
+const articlesJsonName = './articles.json';
+const articlesJson = require(articlesJsonName);
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+let files = articlesJson.files;
+
+// for (let x in files){
+    let stagingPath = `./staging/articles/es/${files[0].path.split("/").pop()}`;
+    let builtFilePath = `./build/articles/es/${files[0].path.split("/").pop()}`;
+    builtFilePath = builtFilePath.replace("md", "html");
+
+    mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
+    razrooMarkdownEngine(stagingPath, builtFilePath).then((output)=>{
+      console.log(output);
+    })
+// }
