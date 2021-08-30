e1747df8258c9ce217cfdc095af462310aa72989
Charlie Greenman <charlie@razroo.com>
2021-08-30 11:45:49 +0300

    Show proof of concept for text summarization.

--- summarize-articles.ts
+++ summarize-articles.ts
@@ -4,16 +4,27 @@ const fs = require('fs');
 const axios = require('axios');
 const articlesJsonName = './articles.json';
 const articlesJson = require(articlesJsonName);
+const SummarizerManager = require("node-summarizer").SummarizerManager;
 import { readFileSync, writeFileSync } from 'fs';
 
 let files = articlesJson.files;
 
 // for (let x in files) {
-    let stagingPath = `./staging/articles/es/${files[0].path.split("/").pop()}`;
+    let builtFilePath = `./build/articles/summary/${files[0].path.split("/").pop()}`;
     let fileString = readFileSync(files[0].path).toString();
 
-    mkdirp.sync(stagingPath.substring(0, stagingPath.lastIndexOf("/")))
-    writeFileSync(stagingPath, response.data.translations[0].text);
+    const Summarizer = new SummarizerManager(fileString,3);
+    Summarizer.getSummaryByRank().then((summary_object)=>{
+        console.log('summary');
+        console.log(summary_object.summary);
+
+        mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
+        writeFileSync(builtFilePath, summary_object.summary);
+    })
+
+    
+
+    
     // razrooMarkdownEngine(response.data.translations[0].text, builtFilePath).then((output)=>{
     //   console.log(output);
     // });
