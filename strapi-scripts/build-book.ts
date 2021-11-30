0feec70c3cdabcc469052af25d6ca8c5f5356333
elishevarom <86791029+elishevarom@users.noreply.github.com>
2021-11-30 14:43:05 +0200

    more logging

--- strapi-scripts/build-book.ts
+++ strapi-scripts/build-book.ts
@@ -3,14 +3,19 @@ const mkdirp = require('mkdirp')
 
 export function buildBookHTML(chapter) {
   let filePathArray = chapter.path.split("/");
+  console.log('chapter %j', chapter)
+  console.log('chapter.path %j', chapter.path);
+  console.log('filePathArray %j', filePathArray);
   let builtFilePath = `./build/book/${filePathArray.pop()}`;
   builtFilePath = builtFilePath.replace("md", "html");
 
   mkdirp.sync(builtFilePath.substring(0, builtFilePath.lastIndexOf("/")))
   let filePathWithoutChapterIdInName = 
     `${filePathArray.slice(0, filePathArray.length - 1).join('/')}/${filePathArray[filePathArray.length - 1].substring(9)}`;
+  console.log('filePathArray.slice(0, filePathArray.length - 1).join', filePathArray.slice(0, filePathArray.length - 1).join('/'));
+  console.log('filePathArray[filePathArray.length - 1].substring(9)', filePathArray[filePathArray.length - 1].substring(9));
+  console.log('filePathArray[filePathArray.length - 1]', filePathArray[filePathArray.length - 1]);
   console.log('filePathWithoutChapterIDInName %j', filePathWithoutChapterIdInName);
-  console.log('builtFilePath %j', builtFilePath);
   razrooMarkdownEngine(`${filePathWithoutChapterIdInName}`, builtFilePath).then((output)=>{
     console.log(output);
   })
